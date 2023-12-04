a5bc7f5615f14a01bf198efbfc41c86eb78fa1cc
lfuruno- <luizafuruno@gmail.com>
2023-12-04 13:09:11 -0300

    Projeto em andamento

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,51 @@
 /*   By: lfuruno- <lfuruno-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:17:02 by lfuruno-          #+#    #+#             */
-/*   Updated: 2023/11/30 22:46:56 by lfuruno-         ###   ########.fr       */
+/*   Updated: 2023/12/04 13:06:52 by lfuruno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*rest_line(int fd, size_t line_bytes
 
+static char	*cut_line(int fd, size_t line_bytes)
 
-char	*get_next_line(int fd)
+static char	read_file(int fd, char *line)
 {
-	size_t	line_bytes;
-	char	*line;
-	char	*read_output;
-	static char	*rest;
+	int		line_bytes
+	char	*buf;
 
-	if (fd = 0 || BUFFEZ_SIZE <= 0)
+	line_bytes = 0;
+	buf = (char *)malloc ((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
 		return (NULL);
-	line_bytes = read_line(fd, *read_output, *rest)
-	if (!read_output)
-		return (NULL);
-	line = cut_line(fd, line_bytes);
-	rest = rest_line(fd, line_bytes);
+	while (line_bytes > 0 && !ft_strchr(line, '\n'))
+	{
+		line_bytes = read(fd, *buf, BUFFER_SIZE);
+		if (line_bytes < 0)
+		{
+			free(buf)
+			return (NULL);
+		}
+		buf[line_bytes] = '\0';
+		line = ft_strjoin(line, buffer);
+	}
+	free(buf)
 	return (line);
 }
 
+char	*get_next_line(int fd)
+{
+	static char	*file;
+	char		*line;
+
+	if (fd = 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	file = read_line(fd, file);
+	if (!file)
+		return (NULL);
+	line = cut_line(file);
+	file = rest_line(file);
+	return (line);
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,56 @@
 /*   By: lfuruno- <lfuruno-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:17:53 by lfuruno-          #+#    #+#             */
-/*   Updated: 2023/11/28 11:17:57 by lfuruno-         ###   ########.fr       */
+/*   Updated: 2023/12/04 13:07:34 by lfuruno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+static 
+
+static char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*p;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	p = (char *)malloc ((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!p)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		p[i] = s2[j];
+		i++;
+		j++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+static char	*ft_strchr(const char *str, int ch)
+{
+	unsigned char	*end_str;
+
+	end_str = ((unsigned char *) str);
+	while (*end_str)
+		end_str++;
+	if (ch == '\0')
+		return ((char *) end_str);
+	while (*str)
+	{
+		if (*str == ch)
+			return ((char *) str);
+		str++;
+	}
+	return (0);
+}
