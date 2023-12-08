7d97c65e19d9e5b419273419a8ac453538ab3f02
lfuruno- <luizafuruno@gmail.com>
2023-12-07 22:13:44 -0300

    Bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,24 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lfuruno- <lfuruno-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/07 17:04:41 by lfuruno-          #+#    #+#             */
-/*   Updated: 2023/12/07 17:04:45 by lfuruno-         ###   ########.fr       */
+/*   Created: 2023/12/07 21:53:20 by lfuruno-          #+#    #+#             */
+/*   Updated: 2023/12/07 21:54:48 by lfuruno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <fcntl.h>
-#include <stdio.h>
-int	main(void)
-{
-	int	fd;
-	fd = open("file.txt", O_RDONLY);
-	printf("%s", get_next_line(fd)); // Never gonna give you up
-	printf("%s", get_next_line(fd)); // Never gonna let you down
-	printf("%s", get_next_line(fd)); // Never gonna run around and desert you
-	close(fd);
-}
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+int		ft_strlen(const char *s);
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *str, int ch);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfuruno- <lfuruno-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 21:55:11 by lfuruno-          #+#    #+#             */
+/*   Updated: 2023/12/07 21:56:21 by lfuruno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*p;
+
+	i = 0;
+	p = malloc ((ft_strlen(s) + 1) * sizeof (char));
+	if (!p)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		p[i] = s[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*p;
+	size_t	i;
+	size_t	j;
+
+	if (!s1)
+		s1 = ft_strdup("");
+	i = 0;
+	j = 0;
+	p = (char *)malloc ((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!p)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		p[i] = s2[j];
+		i++;
+		j++;
+	}
+	p[i] = '\0';
+	free(s1);
+	return (p);
+}
+
+char	*ft_strchr(const char *str, int ch)
+{
+	unsigned char	*end_str;
+
+	if (!str)
+		return (NULL);
+	end_str = ((unsigned char *) str);
+	while (*end_str)
+		end_str++;
+	if (ch == '\0')
+		return ((char *) end_str);
+	while (*str)
+	{
+		if (*str == ch)
+			return ((char *) str);
+		str++;
+	}
+	return (NULL);
+}
